5717a8055ac288ed12176bab24afbb8a937e4512
xovest <79008917+xovest@users.noreply.github.com>
2022-05-05 22:11:36 +0300

    index

--- see.py
+++ see.py
@@ -1,19 +1,10 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
-# Defining the home page of our site
-@app.route("/")  # this sets the route to this page
-def home():
-	return "Hello! this is the main page <h1>HELLO</h1>"  # some basic inline html
-
 @app.route("/<name>")
-def user(name):
-  return f"Hello {name}!"
-
-@app.route("/admin")
-def admin():
-	return redirect(url_for("home"))
+def home(name):
+  return render_template("index.html", content=["some", "thing", "wrong"])
 
 if __name__ == "__main__":
   app.run()
