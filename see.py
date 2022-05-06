c875adef04f6e0197dce767c243e9447bae58bc2
xovest <79008917+xovest@users.noreply.github.com>
2022-05-06 23:50:20 +0300

    bootstrap

--- see.py
+++ see.py
@@ -2,9 +2,13 @@ from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
-@app.route("/<name>")
-def home(name):
-  return render_template("index.html", content=["some", "thing", "wrong"])
+@app.route("/")
+def home():
+  return render_template("index.html")
+
+@app.route("/test")
+def test():
+  return render_template("new.html")
 
 if __name__ == "__main__":
-  app.run()
+  app.run(debug=True)
